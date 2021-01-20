8d1e5bfeed9b40fa44f5ace09f4f41979f845121
EDGARD DE OLIVEIRA MOURA SANTOS <75489160+EdgardMoura@users.noreply.github.com>
2021-01-20 19:42:11 -0300

    Rename
    Changing name.

--- Pensionato/Pensionato/QuartosAlugados.cs
+++ Pensionato/Pensionato/QuartosAlugados.cs
@@ -11,18 +11,18 @@ namespace Pensionato
     {
         public string Name { get; set; }
         public string Email { get; set; }
-        public int Quarto { get; set; }
+        public int Bedroom { get; set; }
     
     public QuartosAlugados(string name, string mail, int bedroom) { 
     
         Name = name;
         Email = mail;
-        Quarto = bedroom;
+        Bedroom = bedroom;
     }
 
         public override string ToString()
         {
-            return Quarto + ": " + Name + ", " +Email;
+            return Bedroom + ": " + Name + ", " +Email;
         }
     }
 }
